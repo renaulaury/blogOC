9d582aafd4cfe05d8dcba625e31332270a25716a
Lily <112404773+Belywen@users.noreply.github.com>
2024-12-27 18:32:55 +0100

    update folder complete

--- src/addComment.php
+++
@@ -1,5 +0,0 @@
-<?php
-if (isset($_POST['submit'])) {
-    $title = input_post(INPUT_POST, 'title', FILTER_SANITIZE_FULL_SPECIAL_CHARS);
-    $content = input_post(INPUT_POST, 'content', FILTER_SANITIZE_FULL_SPECIAL_CHARS);
-}
